afc89be032d05aaee70f09f74fec811c9b117912
Avula Ankith Reddy <44273591+avulaankith@users.noreply.github.com>
2023-03-05 20:06:04 -0600

    Time: 12 ms (45.90%), Space: 9.7 MB (16.36%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        set<ListNode*>s;
+        while(head!=NULL){
+            if(s.find(head)!=s.end()){
+                return head;
+                // auto pos = s.find(head);
+                // int count = 0;
+                // for(auto it = s.begin();it!=s.end();it++){
+                //     if(*pos == *it){
+                //         return count;
+                //     }
+                //     count++;
+                // }
+            }
+            s.insert(head);
+            head=head->next;
+        }
+        return NULL;
+    }
+};
