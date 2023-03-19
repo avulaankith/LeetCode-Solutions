790f753da5a06cd898215ce7fa8c8274352c41a8
Avula Ankith Reddy <44273591+avulaankith@users.noreply.github.com>
2023-03-18 21:54:31 -0500

    Time: 11655 ms (22.69%), Space: 78 MB (65.90%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,27 +1,39 @@
+class Node:
+    def __init__(self) -> None:
+        self.children = {}
+        self.flag = False
+
 class WordDictionary:
 
     def __init__(self):
-        self.dic = []
+        self.root = Node()
 
     def addWord(self, word: str) -> None:
-        self.dic.append(word)
-        
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = Node()
+            node = node.children[char]
+        node.flag = True
 
     def search(self, word: str) -> bool:
-        if '.' not in word:
-            return word in self.dic
+        # node = self.root
 
-        for t in self.dic:
-            if len(t) != len(word):
-                continue
-            for i in range(len(word)):
-                if word[i] == '.':
-                    continue
-                if word[i] != t[i]:
-                    break
-            else:
-                return True
-        return False
+        def dfs(idx, root):
+            node = root
+            for i in range(idx,len(word)):
+                if word[i] == ".":
+                    for child in node.children:
+                        if dfs(i+1, node.children[child]):
+                            return True
+                    return False
+                else:
+                    if word[i] not in node.children:
+                        return False
+                    node = node.children[word[i]]
+            return node.flag
+        
+        return dfs(0, self.root)
 
 
 # Your WordDictionary object will be instantiated and called as such:
