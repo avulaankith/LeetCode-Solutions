0f913a895d42ac103c0995ca56ab6d7f0ef889ee
Avula Ankith Reddy <44273591+avulaankith@users.noreply.github.com>
2023-03-20 01:49:28 -0500

    Time: 63 ms (73.01%), Space: 14 MB (88.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,20 +7,37 @@ class Solution:
             return 0
         elif len(s) == 1:
             return 1
-        string = ""
-        maxLength = 0
 
-        for c in list(s):
-            current = "".join(c)
+        l = 0
+        maxLength = -1
 
-            if current in string:
-                string = string[string.index(current)+1:]
+        charset = set()
 
-            string += c
-            maxLength = max(maxLength,len(string))
+        for r in range(len(s)):
+            if s[r] in charset:
+                while(l < r and s[r] in charset):
+                    charset.remove(s[l])
+                    l += 1
+            charset.add(s[r])
+            maxLength = max(maxLength,r-l+1)
         return maxLength
+        
 
+        # Optimized Solution 2
+        # string = ""
+        # maxLength = 0
 
+        # for c in list(s):
+        #     current = "".join(c)
+
+        #     if current in string:
+        #         string = string[string.index(current)+1:]
+
+        #     string += c
+        #     maxLength = max(maxLength,len(string))
+        # return maxLength
+
+        # Naive Solution
         # for i in range(len(s)):
         #     for j in range(i, len(s)):
         #         substr = s[i:j+1]
