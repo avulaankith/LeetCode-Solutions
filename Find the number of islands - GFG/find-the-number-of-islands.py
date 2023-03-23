1b21715a1a180a6aec5aa02aa19d399fc789b0a8
Avula Ankith Reddy <44273591+avulaankith@users.noreply.github.com>
2023-03-23 03:29:20 -0500

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.py
+++ Find the number of islands - GFG/find-the-number-of-islands.py
@@ -5,21 +5,6 @@ sys.setrecursionlimit(10**8)
 class Solution:
     def numIslands(self,grid):
         #code here
-        def dfs(grid ,i, j, visited):
-            n = len(grid)
-            m = len(grid[0])
-            rows = [-1, -1, -1,  0, 0,  1, 1, 1]
-            cols = [-1,  0,  1, -1, 1, -1, 0, 1]
-            visited[i][j] = 1
-            
-            for k in range(8):
-                nrow = i + rows[k]
-                mcol = j + cols[k]
-                
-                if (nrow >= 0 and nrow < n) and (mcol >= 0 and mcol < m) and (visited[nrow][mcol] == 0 and grid[nrow][mcol] == 1):
-                    dfs(grid, nrow, mcol, visited)
-        
-        
         n = len(grid)
         m = len(grid[0])
         visited = [[0 for j in range(m)] for i in range(n)]
@@ -28,12 +13,24 @@ class Solution:
             for j in range(m):
                 if visited[i][j] == 0 and grid[i][j] == 1:
                     count += 1
-                    dfs(grid, i, j, visited)
+                    self.dfs(grid, i, j, visited)
         return count
                     
         
     
-    
+    def dfs(self, grid ,i, j, visited):
+        n = len(grid)
+        m = len(grid[0])
+        rows = [-1, -1, -1,  0, 0,  1, 1, 1]
+        cols = [-1,  0,  1, -1, 1, -1, 0, 1]
+        visited[i][j] = 1
+        
+        for k in range(8):
+            nrow = i + rows[k]
+            mcol = j + cols[k]
+            
+            if (nrow >= 0 and nrow < n) and (mcol >= 0 and mcol < m) and (visited[nrow][mcol] == 0 and grid[nrow][mcol] == 1):
+                self.dfs(grid, nrow, mcol, visited)
 
 
 #{ 
